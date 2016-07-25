b2c28c15ded838c436293ea8b945e122da064b3c
Xun Li <xunli@asu.edu>
2016-07-25 11:01:20 -0700

    update

--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
 // Author: lixun910 at gmail.com
 
+// simplest MapCola Example
+var MapCola = require('../lib/index.js');
+
+var cmap = new MapCola("mymap", {
+  tileLayer : "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+  width : "800px",
+  height : "600px"
+  });
+
+cmap.AddLayer("test/data/nat.shp", {'fill_color':'red'});
+
+
+// MapCola work with existing LeafletMap project
 var L = require('leaflet'),
-    LeafletMap = require('leaflet-map'),
-    MapCola = require('../lib/index.js').MapCola;
+    LeafletMap = require('leaflet-map');
 
 var map = LeafletMap();
 map.setView([0,0],4);
@@ -10,28 +22,6 @@ map.setView([0,0],4);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
   .addTo(map);
 
-var mapCola = new MapCola();
-var test_url = "test/data/nat.shp";
+var cmap2 = new MapCola(map);
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", test_url, true);
-xhr.responseType = 'arraybuffer';
-xhr.onload = function(evt) {
-  var content = xhr.response;
-  var prj = null;
-  var shp_reader = new MapCola.ShpReader(content);
-  var shp_map = new MapCola.ShpMap("pubhsg", shp_reader, L, map);
-
-  //L.DomUtil.create('Canvas', '', document.body);
-  var elCanvas = document.createElement("Canvas");
-  elCanvas.setAttribute('style', 'position:absolute;top:0;left:0;width:100%;height:100%;pointer-events:none');
-  document.body.appendChild(elCanvas);
-
-  var hlCanvas = document.createElement("Canvas");
-  elCanvas.setAttribute('style', 'position:absolute;top:0;left:0;width:100%;height:100%;');
-  document.body.appendChild(hlCanvas);
-
-  var params = {'fill_color':'red'}
-  var mapCanvas = new MapCola.MapCanvas(shp_map, elCanvas, hlCanvas, params);
-};
-xhr.send(null);
+cmap2.AddLayer("test/data/nat.shp", {'fill_color':'blue'});
