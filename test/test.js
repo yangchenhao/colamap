96b32e0b9773b66f1cc016849eaf09ccf5e204c1
Xun Li <xunli@asu.edu>
2016-07-27 12:31:37 -0700

    update mapcanvas.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 // Author: lixun910 at gmail.com
 
-// simplest MapCola Example
+// 1. simplest MapCola Example
 var MapCola = require('../lib/index.js');
 
 var cmap = new MapCola("mymap", {
@@ -12,16 +12,17 @@ var cmap = new MapCola("mymap", {
 cmap.AddLayer("test/data/nat.shp", {'fill_color':'red'});
 
 
-// MapCola work with existing LeafletMap project
+
+
+// 2. MapCola work with existing LeafletMap project
 var L = require('leaflet'),
     LeafletMap = require('leaflet-map');
 
-var map = LeafletMap();
-map.setView([0,0],4);
+var lmap = LeafletMap().setView([0,0],4);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
-  .addTo(map);
+  .addTo(lmap);
 
-var cmap2 = new MapCola(map);
+var cmap2 = new MapCola(lmap);
 
 cmap2.AddLayer("test/data/nat.shp", {'fill_color':'blue'});
